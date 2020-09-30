9f9274d2b6a508b1c65b9819259bffb5d234db5d
Maia <maia.gov@icloud.com>
2020-10-01 00:55:00 +0100

    Revert "🏓 Extended Ping Command"

--- commands/ping.ts
+++ commands/ping.ts
@@ -1,19 +1,7 @@
 import Discord from 'discord.js';
-import preEmbed from '../helpers/embed'
 
 export default async (msg: Discord.Message) => {
-	// Send initial message
-	const start = Date.now();
-	const initialMessage = await msg.channel.send("🏓 Pinging...")
-	const latency = Date.now() - start;
-
-	// Create embed
-	const embed = preEmbed(false)
-	.setTitle('🏓 Pong!')
-	.addField('Gateway Ping:', `${msg.client.ws.ping}ms`)
-	.addField('API Latency:', `${latency}ms`)
-	
 	// Reply with WS ping
-	await initialMessage.edit('', embed);
+	await msg.reply(`Pong! ${msg.client.ws.ping}ms`);
 	return;
 };
